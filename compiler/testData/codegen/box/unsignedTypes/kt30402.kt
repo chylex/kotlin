4426379fa17a059b8cea1c2c448ac5e2b7430722
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-12-29 16:27:04 +0300

    JVM box tests for KT-30402

---
+++ compiler/testData/codegen/box/unsignedTypes/kt30402.kt
@@ -0,0 +1,13 @@
+// WITH_RUNTIME
+// IGNORE_BACKEND: JVM
+// IGNORE_BACKEND: JS_IR
+
+val unsigned = 0x8fffffffU
+val good = "123 " + unsigned
+val bad = "123 " + 0x8fffffffU
+
+fun box(): String {
+    if (good != "123 2415919103") return "good: '$good'"
+    if (bad != "123 2415919103") return "bad: '$bad'"
+    return "OK"
+}
